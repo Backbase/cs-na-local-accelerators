300bc799dc1c0de3bbf9ca48c0059afb15fe8b9e
John Coombs <johnc@backbase.com>
2022-04-25 13:12:04 -0400

    custom view for security-center

--- libs/journeys/identity-self-service/src/lib/journeys-identity-self-service.module.ts
+++ libs/journeys/identity-self-service/src/lib/journeys-identity-self-service.module.ts
@@ -4,8 +4,16 @@ import {
   IdentitySelfServiceJourneyModule,
   IdentitySelfServiceJourneyConfigurationToken,
   IdentitySelfServiceJourneyConfiguration,
+  IdentitySelfServiceJourneyComponent,
+  UserManageProfileViewComponent,
+  UserLocalizationComponent,
+  UserLocalizationCommunicationsViewComponent,
+  UserLocalizationChangeLanguageViewComponent,
+  DeviceInformationViewComponent,
 } from '@backbase/identity-self-service-journey-ang';
 import { DeepPartial } from '@backbase/identity-common-ang';
+
+import { UserIdentitySecurityCenterWidgetModule } from '@backbase/user-identity-security-center-widget-ang';
 import { UserIdentitySecurityCenterComponent } from './user-identity-security-center/user-identity-security-center.component';
 
 const ProfileConfigProvider: Provider = {
@@ -19,8 +27,59 @@ const ProfileConfigProvider: Provider = {
   } as DeepPartial<IdentitySelfServiceJourneyConfiguration>,
 };
 
+const customRoute = {
+  path: '',
+  component: IdentitySelfServiceJourneyComponent,
+  children: [
+    { path: '', redirectTo: 'profile', pathMatch: 'full' },
+    {
+      path: 'profile',
+      data: {
+        title: $localize`:Tab label for managing user profile@@bb-identity-self-service-journey.tab-user-manage-profile:Profile`,
+      },
+      component: UserManageProfileViewComponent,
+    },
+    {
+      path: 'user-localization',
+      data: {
+        title: $localize`:Tab label for changing user preferences@@bb-identity-self-service-journey.tab-localization:Localization`,
+      },
+      component: UserLocalizationComponent,
+      children: [
+        { path: '', redirectTo: 'communications', pathMatch: 'full' },
+        {
+          path: 'communications',
+          component: UserLocalizationCommunicationsViewComponent,
+        },
+        {
+          path: 'change-language',
+          component: UserLocalizationChangeLanguageViewComponent,
+        },
+      ],
+    },
+    {
+      path: 'login-security',
+      data: {
+        title: $localize`:Tab label for login and security settings@@bb-identity-self-service-journey.tab-login-security:Login & security`,
+      },
+      component: UserIdentitySecurityCenterComponent,
+    },
+    {
+      path: 'devices',
+      data: {
+        title: $localize`:Tab label for managing devices@@bb-identity-self-service-journey.tab-devices:Devices`,
+      },
+      component: DeviceInformationViewComponent,
+    },
+  ],
+};
+
 @NgModule({
-  imports: [CommonModule, IdentitySelfServiceJourneyModule.forRoot()],
+  imports: [
+    CommonModule,
+    IdentitySelfServiceJourneyModule.forRoot({ route: customRoute }),
+    UserIdentitySecurityCenterWidgetModule,
+  ],
   providers: [ProfileConfigProvider],
   declarations: [UserIdentitySecurityCenterComponent],
 })
--- libs/journeys/identity-self-service/src/lib/user-identity-security-center/user-identity-security-center.component.ts
+++ libs/journeys/identity-self-service/src/lib/user-identity-security-center/user-identity-security-center.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserIdentitySecurityCenterViewComponent } from '@backbase/identity-self-service-journey-ang';
 
 @Component({
   selector: 'bb-user-identity-security-center',
   templateUrl: './user-identity-security-center.component.html',
   styleUrls: ['./user-identity-security-center.component.scss'],
 })
-export class UserIdentitySecurityCenterComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class UserIdentitySecurityCenterComponent extends UserIdentitySecurityCenterViewComponent {}
